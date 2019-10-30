707d36aeaf758fe1af93ce7a32e4cacb4cd3c519
Keyno <kofujiw@gmail.com>
2019-10-30 23:41:31 +0900

    add sample db query code.

---
+++ app/refinement/repository/PostRepository.scala
@@ -0,0 +1,19 @@
+package refinement.repository
+
+import refinement.form.Post
+import scalikejdbc._
+import scalikejdbc.jsr310._
+
+object PostRepository {
+
+  def findAll: Seq[Post] = DB readOnly { implicit session =>
+    sql"SELECT id, body, date FROM posts".map { rs =>
+      Post(rs.long("id"), rs.string("body"), rs.offsetDateTime("date"))
+    }.list().apply()
+  }
+
+  def add(post: Post): Unit = DB localTx { implicit session =>
+    sql"INSERT INTO posts (body, date) VALUES (${post.body}, ${post.date})".update().apply()
+  }
+}
+
