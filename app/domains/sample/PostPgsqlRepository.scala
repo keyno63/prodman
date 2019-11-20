d4a73b29da13387eee9442e3233b9567717ee95b
Keyno <kofujiw@gmail.com>
2019-11-21 00:06:27 +0900

    fix db impl.

--- app/domains/sample/PostPgsqlRepository.scala
+++ app/domains/sample/PostPgsqlRepository.scala
@@ -7,18 +7,13 @@ import scalikejdbc.jsr310._
 
 object PostPgsqlRepository {
 
-  //DBsWithEnv("prod").setup('sandbox)
-  DBs.setupAll()
-
-  def findAll: Seq[Post] = NamedDB("pgsql") readOnly { implicit session =>
-  //def findAll: Seq[Post] = DB readOnly { implicit session =>
+  def findAll: Seq[Post] = NamedDB('pgsql) readOnly { implicit session =>
     sql"SELECT name, date FROM company".map { rs =>
       Post(rs.long("id"), rs.string("body"), rs.offsetDateTime("date"))
     }.list().apply()
   }
 
-  def add(post: Post): Unit = NamedDB("pgsql") localTx { implicit session =>
-  //def add(post: Post): Unit = DB localTx { implicit session =>
+  def add(post: Post): Unit = NamedDB('pgsql) localTx { implicit session =>
     sql"INSERT INTO posts (body, date) VALUES (${post.body}, ${post.date})".update().apply()
   }
 
