2de4db450e7170ef5a0b6614e24f428650e537f5
Keyno <kofujiw@gmail.com>
2019-11-25 23:34:23 +0900

    fix db impl.

--- app/controllers/sample/DBController.scala
+++ app/controllers/sample/DBController.scala
@@ -41,12 +41,23 @@ class DBController @Inject()(cc: ControllerComponents, override val messagesApi:
     form.bindFromRequest.fold(
       error => {
         val errorMessage = Messages(error.errors("post").head.message)
-        BadRequest(Response(Meta(400, Some(errorMessage))))
+        BadRequest(
+          Json.toJson(
+            Response(
+              Meta(400),
+              Some(
+                Json.obj("error" -> Json.toJson(errorMessage))
+              )
+            )
+          )
+        )
       },
       postRequests => {
         val post = Post(postRequests.body, OffsetDateTime.now)
         PostPgsqlRepository.add(post)
-        Ok(Response(Meta(200)))
+        Ok(
+          Json.toJson(Response(Meta(200)))
+        )
       }
     )
   }
